a8b5ed33fbbd3d0fb1f32ee593e466dc2606c496
Jhonatapazos06 <jhonatanpazos240@gmail.com>
2025-10-14 20:48:33 -0500

    primer commit

---
+++ 1_tipos_de_variable.py
@@ -0,0 +1,27 @@
+# ejemplo de datos tipo INT
+num = 3
+
+# ejemplo de datos tipo float
+decimal = 3.5
+
+# ejemplo de dato tipo string
+texto = "hola mundo"
+
+# ejemplo de datos tipo boolean
+is_true = True
+is_false = False
+
+#type es una funcion que nos permite conocer el tipo de datos de una variable
+#print se visualiza en la termina 
+print(type(num))
+print(type(decimal))
+print(type(texto))
+print(type(is_false))
+
+
+int
+float
+str
+bool
+
+# esto es un cometrio
---
+++ 2_variables.py
@@ -0,0 +1,25 @@
+num1 = 3
+num2 = 4
+suma = num1+num2
+
+print(suma)
+
+num1 = 5.5
+num2 = 5.5
+suma = num1+num2
+print(suma)
+
+# f-muestra el valor de la variable eje: el resultado de la suma
+print(f"el resultadoes {suma}")
+
+num1 = int(input("ingrese un numero"))
+num2 = int(input("ingrese otro numero"))
+
+suma = num1+num2
+print(f"la suma es: {suma}")
+num3=int(input("ingrese un numero"))
+num4=int(input("ingrese otro numero"))
+
+resta=num3-num4
+print(f"la resta es: {resta}")
+
